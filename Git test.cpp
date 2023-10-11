4cb3bd136a9a821e1ce2a52b0c82636e60038ec9
k023g0015@g.neec.ac.jp <k023g0015@g.neec.ac.jp>
2023-10-11 14:34:17 +0900

    日付追加

--- Git test.cpp
+++ Git test.cpp
@@ -8,6 +8,8 @@ int main()
     std::cout << "MNSTHello!!!!!!!\n";
 
     std::cout << "Hello!!!!!!!\n";
+
+    std::cout << "2023,10,11\n";
 }
 
 // プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
